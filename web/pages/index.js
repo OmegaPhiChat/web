ca7157bb1d5429161322fd727a308654d0a29f2d
Kevin <kevin@KevinHuynh.localdomain>
2023-10-02 17:17:29 -0400

    created the landing page presentable on mobile

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,10 +1,28 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import styles from '../styles/index.module.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <p>Hello World</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Omega Phi Chat</h1>
+      <div calssName={styles.inputContainer}>
+        <label className={styles.label} htmlFor="userInput"></label>
+        <div className={styles.inputWrapper}>
+          <input
+            type="text"
+            id="userInput"
+            name="userInput"
+            placeholder="Enter Username"
+            className={styles.input}
+            />
+            <div className={styles.buttonContainer}>
+              <button className={styles.button}>Phi Time</button>
+              </div>
+          </div>
+      </div>
+    </div>
   )
 }
