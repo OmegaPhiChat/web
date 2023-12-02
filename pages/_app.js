2fafafb18c5ce9081744474fc44e934fc6f1c276
kphan32 <kavin@kphan.tech>
2023-12-01 22:16:19 -0500

    Configure API URL env var

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
+import { API_URL } from "@/api";
+
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
