e634c53de55f614e7f0b6fdf0f3b518b7c485e5b
Kevin <Kevinhuynh7070@gmail.com>
2023-12-01 23:35:38 -0500

    Created function to send a username request

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
 import styles from "../styles/index.module.css";
+import { API_URL } from "@/api";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+
+  const handleUsernameSubmit = async () => {
+    const response = await fetch(`${API_URL}/join?username=${username}`, {
+      method: "POST",
+    });
+
+    if (response.ok) {
+      router.push("/chat");
+    } else {
+      console.error("failed to join phi chat: ", response.statusText);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.mainTitle}>Omega Phi Chat</h1>
@@ -13,9 +31,16 @@ export default function Home() {
             name="userInput"
             placeholder="Enter Username"
             className={styles.mainInput}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <div className={styles.submitContainer}>
-            <button className={styles.mainSubmit}>Phi Time</button>
+            <button
+              className={styles.mainSubmit}
+              onClick={handleUsernameSubmit}
+            >
+              Phi Time
+            </button>
           </div>
         </div>
       </div>
